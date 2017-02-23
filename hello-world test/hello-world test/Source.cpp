414e1fbc06e446d2c57db17767a840c03292f60e
Demet <Demet@DESKTOP-Q9670JU>
2017-02-23 17:22:23 -0500

    Testing out how to upload code.

---
+++ hello-world test/hello-world test/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main() {
+	std::string a = "hello";
+	std::string b = "world";
+
+	std::cout << a << " " << b << std::endl;
+	std::cout << "This is not working the way I want.";
+
+	system("pause");
+}
